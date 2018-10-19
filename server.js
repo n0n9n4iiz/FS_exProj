20f6a9b373f93556cf561176318804f1602385c2
krittayot <nai-iiz@hotmail.co.th>
2018-10-19 19:21:49 +0700

    sql value+s

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post('/products/addnew',function(req,res){
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
-    var sql = `insert into products (id,title,price) value ('${id}','${title}','${price}')`;
+    var sql = `insert into products (id,title,price) values ('${id}','${title}','${price}')`;
       db.any(sql)              
        .then(function(data){ //ทำหลังจากดีงฐานข้อมูล
            res.render('pages/products');
