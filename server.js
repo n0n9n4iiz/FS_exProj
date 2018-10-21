7f6c9b2dd3f8562620d5386fb6da14bbbb15fd0f
krittayot <nai-iiz@hotmail.co.th>
2018-10-22 06:50:37 +0700

    localtime again

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ app.get('/products/:pid', function (req, res) {
 
     var pid = req.params.pid;
     var sql = "select * from products where id=" + pid;
-    var time = moment().local().format('YYYY-MM-DD HH:MM:SS');
+    var time  = moment.utc().toDate();
+    time = moment(localTime).format('YYYY-MM-DD HH:mm:ss');
     db.any(sql)
         .then(function (data) {
 
